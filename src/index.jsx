e4ba4710155efcbf3395bcc3ad6d3b549de95d5f
Devendra Negi <devnegi_kec@yahoo.co.in>
2016-05-11 22:31:31 +0530

    Todo React component with Redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,99 @@
-import {createStore} from 'redux';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore, combineReducers} from 'redux';
 
-const counter = (state = 0, action) =>{
-    switch (action.type){
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMENT':
-            return state -1;
-        default:
+const todo = (state, action) => {
+  switch (action.type)  {
+      case 'ADD_TODO':
+        return {
+          id: action.id,
+          text: action.text,
+          completed: false  
+        };
+      case 'TOGGLE_TODO':
+        if (state.id !== action.id){
             return state;
+        }
+        return {
+            ...state,
+            completed: !state.completed
+        };
+      default:
+      return state;
+  }
+};
+
+const todos = (state = [], action) => {
+  switch (action.type)  {
+      case 'ADD_TODO':
+        return [
+            ...state,
+            todo(undefined, action)
+        ];
+      case 'TOGGLE_TODO':
+        return state.map(t => 
+            todo(t, action)
+        );
+      default:
+      return state;
+  }
+};
+
+ const visibilityFilter = (
+     state = 'SHOW_ALL', 
+     action
+ ) => {
+  switch (action.type)  {
+      case 'SET_VISIBILITY_FILTER':
+        return  action.filter;
+      default:
+      return state;
+  }
+};
+
+
+const todoApp = combineReducers({
+   todos,
+   visibilityFilter 
+});
+
+const store = createStore( todoApp );
+
+let nextTodoId = 0; 
+class TodoApp extends React.Component {
+    render() {
+        return (
+            <div>
+                <button onClick={()=> {
+                    store.dispatch({
+                        type: 'ADD_TODO',
+                        text: 'Test',
+                        id: nextTodoId ++
+                    });
+                }}>
+                Add Todo 
+                </button>
+                <ul>
+                    {this.props.todos.map(todo =>
+                        <li key={todo.id}>
+                            {todo.text}
+                        </li>   
+                     )}
+                </ul>
+            </div>
+        );
     }
 }
-
-const store = createStore( counter );
 const render = () => {
-    document.body.innerText = store.getState();
-}
+    ReactDOM.render(
+      <TodoApp 
+        todos={store.getState().todos}
+      />,
+        document.getElementById('app')
+    );    
+};
+
 
 store.subscribe(render);
-render();
 
-document.addEventListener('click', ()=> {
-    store.dispatch({ type: 'INCREMENT'});
-});
+render();
